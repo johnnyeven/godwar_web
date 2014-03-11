b382e638ebe4dafc5c14ebec927f04c520af703f
johnnyeven_home <johnnyeven@gmail.com>
2014-03-12 00:43:40 +0800

    1

--- admin/application/controllers/import.php
+++ admin/application/controllers/import.php
@@ -34,6 +34,7 @@ class Import extends CI_Controller
 		$this->load->library('upload', $config);
 		if (!$this->upload->do_upload($fileElementName)) {
 			$error = $this->upload->display_errors('<stronng>', '</stronng>');
+			echo $error;
 		} else {
 			$data = $this->upload->data();
 			$fileName = $uploadDir . '/' . $data['file_name'];
