dfdf07d7678677659e5dc9dc13279f938b33fd0b
johnnyeven_home <johnnyeven@gmail.com>
2014-03-11 23:47:55 +0800

    1

--- app/application/config/base.config.php
+++ app/application/config/base.config.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 //每秒恢复血量
-$config['base_recover_health_rate'] = 8;
+$config['base_recover_health_rate'] = 1;
 
 //固定休息时间
 $config['base_battle_rest_time'] = 30;
