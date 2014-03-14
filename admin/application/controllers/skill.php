cc3c41053a981298e842c1a8887b40151cbd449d
johnnyeven_work <johnnyeven@gmail.com>
2014-03-14 17:06:24 +0800

    1

--- admin/application/controllers/skill.php
+++ admin/application/controllers/skill.php
@@ -14,38 +14,45 @@ class Skill extends CI_Controller
 	{
 		$result = array(
 			array(
-				'id'			=>	'220001',
-				'name'			=>	'火球术',
-				'comment'		=>	'对目标造成120%的魔法伤害，30%概率额外造成50%的魔法伤害',
+				'id'			=>	'230001',
+				'name'			=>	'祈祷术',
+				'comment'		=>	'防御加成10%，持续3轮',
 				'level_limit'	=>	10,
 				'skill_limit'	=>	''
 			),
 			array(
-				'id'			=>	'220002',
-				'name'			=>	'雷电术',
-				'comment'		=>	'对所有目标造成100%的魔法伤害',
-				'level_limit'	=>	17,
+				'id'			=>	'230002',
+				'name'			=>	'圣光术',
+				'comment'		=>	'对目标造成120%魔法伤害，如果目标生命值低于50%，则造成150%魔法伤害',
+				'level_limit'	=>	15,
 				'skill_limit'	=>	''
 			),
 			array(
-				'id'			=>	'220003',
-				'name'			=>	'寒冰盾',
-				'comment'		=>	'受到的伤害降低10%，并且由攻击者承受，持续两轮',
+				'id'			=>	'230003',
+				'name'			=>	'庇护',
+				'comment'		=>	'对队友及自己防御加成5%、魔防加成5%、闪避加成5%',
 				'level_limit'	=>	20,
 				'skill_limit'	=>	''
 			),
 			array(
-				'id'			=>	'220004',
-				'name'			=>	'法力专精',
+				'id'			=>	'230004',
+				'name'			=>	'圣言专精',
 				'comment'		=>	'被动技能，魔法伤害提升2%',
 				'level_limit'	=>	25,
 				'skill_limit'	=>	''
 			),
 			array(
-				'id'			=>	'220005',
-				'name'			=>	'轻甲专精',
+				'id'			=>	'230005',
+				'name'			=>	'皮甲专精',
 				'comment'		=>	'被动技能，防御加成2%',
 				'level_limit'	=>	27,
+				'skill_limit'	=>	''
+			),
+			array(
+				'id'			=>	'230006',
+				'name'			=>	'治疗术',
+				'comment'		=>	'恢复自己生命值4%',
+				'level_limit'	=>	29,
 				'skill_limit'	=>	''
 			)
 		);
