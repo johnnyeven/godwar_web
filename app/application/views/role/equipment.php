b71366b0ca94ea43acd5ca434433048c7044785e
johnnyeven_home <johnnyeven@gmail.com>
2014-03-21 23:50:06 +0800

    1

--- app/application/views/role/equipment.php
+++ app/application/views/role/equipment.php
@@ -9,9 +9,33 @@
         <p>项链：</p>
     </div>
     <div class="right">
-    	<?php foreach($equipments as $equipment): ?>
-        <div class="equipment" style="padding:5px 20px;float:left;min-width:120px;"><?php echo $equipment['name']; ?></div>
-        <?php endforeach; ?>
+        <?php
+        foreach($equipments as $equipment)
+        {
+            echo '<div class="equipment" style="padding:5px 20px;float:left;min-width:120px;">';
+            if($equipment['grade'] == 0)
+            {
+                echo '<span style="color:#000000">' . $equipment['name'] . '</span>';
+            }
+            elseif($equipment['grade'] == 1)
+            {
+                echo '<span style="color:#0099FF">' . $equipment['name'] . '</span>';
+            }
+            elseif($equipment['grade'] == 2)
+            {
+                echo '<span style="color:#2BD52B">' . $equipment['name'] . '</span>';
+            }
+            elseif($equipment['grade'] == 3)
+            {
+                echo '<span style="color:#CC00FF">' . $equipment['name'] . '</span>';
+            }
+            elseif($equipment['grade'] == 4)
+            {
+                echo '<span style="color:#FF9900">' . $equipment['name'] . '</span>';
+            }
+            echo '</div>';
+        }
+        ?>
         <div style="clear:both;"></div>
     </div>
     <div class="clear"></div>
