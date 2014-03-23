b9abaea785c01bc4418b56ba703fcce8acc450a1
johnnyeven_home <johnnyeven@gmail.com>
2014-03-23 18:57:11 +0800

    1

--- app/application/models/utils/check_user.php
+++ app/application/models/utils/check_user.php
@@ -79,8 +79,7 @@ class Check_user extends CI_Model
 			
 			include_once APPPATH . 'libraries/RoleAdapter.php';
 			$role_adapter = new RoleAdapter($id);
-			var_dump($role_adapter);
-			exit();
+			
 			if($role_adapter->is_init)
 			{
 				return $role_adapter;
