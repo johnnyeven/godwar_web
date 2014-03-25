d3c3985f74cf09eefa13df0efd5cc0edaf1bae5d
johnnyeven_home <johnnyeven@gmail.com>
2014-03-25 23:22:29 +0800

    1

--- app/application/views/action/market_sell.php
+++ app/application/views/action/market_sell.php
@@ -2,7 +2,7 @@
     
     </div>
     <div class="right">
-        <form action="" method="post" enctype="application/x-www-form-urlencoded" name="form" id="form">
+        <form action="<?php echo site_url('action/market/sell_submit'); ?>" method="post" enctype="application/x-www-form-urlencoded" name="form" id="form">
         	<p>名字：<?php echo $equipment['name']; ?></p>
             <p>价格：<input type="text" name="price" id="price" value="<?php echo $equipment['price']; ?>" /></p>
             <p><input type="submit" name="button" id="button" value="提交" /></p>
