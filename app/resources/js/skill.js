ad6a83c775515b9d4c7f7a2ff429bd17a62038d6
johnnyeven_home <johnnyeven@gmail.com>
2014-03-26 23:12:48 +0800

    1

--- app/resources/js/skill.js
+++ app/resources/js/skill.js
@@ -9,9 +9,9 @@ $(function() {
 		dup.attr("id", "job_" + id);
 
 		if($("#job_" + pre_job).length > 0) {
-			$("#job_" + pre_job).find("div.sub_job").append(dup);
+			$("#job_" + pre_job + " > div.sub_job").append(dup);
 			console.log("#job_" + pre_job + "添加");
-			console.log($("#job_" + pre_job).find("div.sub_job").length);
+			console.log($("#job_" + pre_job + " > div.sub_job").length);
 		}
 		else
 		{
