1f1dd236bc0d9f1396414ffbe9a91af6b4ad6eb1
johnnyeven_work <johnnyeven@gmail.com>
2014-03-31 17:13:35 +0800

    1

--- app/application/controllers/role/gift.php
+++ app/application/controllers/role/gift.php
@@ -30,4 +30,56 @@ class Gift extends CI_Controller
 		$this->load->model ( 'utils/render' );
 		$this->render->render ( $this->pageName, $parameter );
 	}
+
+	public function activate($id)
+	{
+		if(!empty($id))
+		{
+			$this->load->library('Mongo_db');
+			$parameter = array(
+				'id'	=>	intval($this->currentRole->role['race'])
+			);
+			$result = $this->mongo_db->where($parameter)->get('race');
+			$exist = false;
+			$level_limit = 0;
+			if(!empty($result))
+			{
+				$result = $result[0];
+				foreach($result['gift'] as $gift)
+				{
+					if($gift['id'] == $id)
+					{
+						$exist = true;
+						$level_limit = $gift['level_limit'];
+						break;
+					}
+				}
+
+				if(!$exist)
+				{
+					showMessage( MESSAGE_TYPE_ERROR, 'GIFT_ACTIVATE_ERROR_NOT_EXIST', '', 'role/gift', true, 5 );
+				}
+
+				if($this->currentRole->role['level'] >= $level_limit)
+				{
+					array_push($this->currentRole->role['gift'], $id);
+					$this->currentRole->save();
+
+					showMessage( MESSAGE_TYPE_SUCCESS, 'GIFT_ACTIVATE_SUCCESS', '', 'role/gift', true, 5 );
+				}
+				else
+				{
+					showMessage( MESSAGE_TYPE_ERROR, 'GIFT_ACTIVATE_ERROR_LEVEL_LIMIT', '', 'role/gift', true, 5 );
+				}
+			}
+			else
+			{
+				showMessage( MESSAGE_TYPE_ERROR, 'GIFT_ACTIVATE_ERROR_RACE_ERROR', '', 'role/gift', true, 5 );
+			}
+		}
+		else
+		{
+			showMessage( MESSAGE_TYPE_ERROR, 'GIFT_ACTIVATE_ERROR_NO_PARAM', '', 'role/gift', true, 5 );
+		}
+	}
 }
