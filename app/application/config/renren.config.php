6109921de8fa7716c955083f82c806f16c77c92b
johnnyeven_work <johnnyeven@gmail.com>
2014-04-04 15:23:47 +0800

    1

--- app/application/config/renren.config.php
+++ app/application/config/renren.config.php
@@ -2,4 +2,4 @@
 
 $config['renren_app_key'] = 'c28579965f4d482694e5644e61ec0453';
 $config['renren_app_secret'] = '6f57a845149347998c9c52fbe408f938';
-$config['renren_auth_callback'] = 'http://localhost/godwar_web/app/connect/renren/auth_callback';
+$config['renren_auth_callback'] = 'http://gw.profzone.net/app/connect/renren/auth_callback';
